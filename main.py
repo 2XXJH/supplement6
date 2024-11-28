ce76b54efacf95e32fc0b308758c1d5df6395abe
JH2XX <158313400+JH2XX@users.noreply.github.com>
2024-11-28 16:11:58 -0500

    doc: added documentation for generate normal array

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import numpy as np
 
 def generate_normal_array(mean, std_dev, shape):
+    """
+    Generates a numpy array of normally distributed numbers.
+    :param mean: Mean of the distribution
+    :param std_dev: Standard deviation of the distribution
+    :param shape: Shape of the array (tuple)
+    :return: Numpy array of normally distributed numbers
+    """ 
     return np.random.normal(mean, std_dev, shape)
 
 
